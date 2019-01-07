883c01a9bf8727718b45f0817332b6d5be8e0879
jheinz1999 <jonathan.heinz@aol.com>
2019-01-07 12:35:57 -0800

    GET request at /api/posts endpoint returns correct data

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const server = require('express')();
 
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+
+  db.find()
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }));
+
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`A rad server is running on port ${port}`));
