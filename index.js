43fcbfdf9ca1808f5d3810c99edb86ad2f8977ff
jheinz1999 <jonathan.heinz@aol.com>
2019-01-07 12:39:46 -0800

    GET request at /api/posts/:id returns correct data

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ server.get('/api/posts', (req, res) => {
 
 });
 
+server.get('/api/posts/:id', (req, res) => {
+
+  db.findById(req.params.id)
+    .then(data => data.length > 0 ? res.status(200).json(data) : res.status(404).json({ message: "The post with the specified ID does not exist." }))
+    .catch(err => res.status(500).json({ error: "The post information could not be retrieved." }));
+
+});
+
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`A rad server is running on port ${port}`));
